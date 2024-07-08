a9f291d1286341be2589e344bb230619be685749
jaiaakashg <jaiaakashg28210@gmail.com>
2024-07-08 16:50:16 +0530

    Add files via upload

---
+++ night_sky_using_oled.ino
@@ -0,0 +1,41 @@
+#include <Wire.h>
+#include <Adafruit_GFX.h>
+#include <Adafruit_SSD1306.h>
+
+#define SCREEN_WIDTH 128 
+#define SCREEN_HEIGHT 64 
+#define OLED_RESET    -1
+Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET);
+
+#define NUM_STARS 50
+int stars[NUM_STARS][2];
+
+void setup() {
+  Serial.begin(115200);
+
+ 
+  if(!display.begin(SSD1306_SWITCHCAPVCC, 0x3C)) {
+    Serial.println(F("SSD1306 allocation failed"));
+    for(;;);
+  }
+
+  for (int i = 0; i < NUM_STARS; i++) {
+    stars[i][0] = random(SCREEN_WIDTH);
+    stars[i][1] = random(SCREEN_HEIGHT);
+  }
+
+  display.clearDisplay();
+}
+
+void loop() {
+  display.clearDisplay();
+
+  for (int i = 0; i < NUM_STARS; i++) {
+    int brightness = random(0, 256);
+    display.drawPixel(stars[i][0], stars[i][1], brightness > 128 ? SSD1306_WHITE : SSD1306_BLACK);
+  }
+
+  display.display();
+  delay(100);
+}
+
